479e21f420e3cbe5e6c05d1c4825d7d438d199af
Roman Khassraf <roman@khassraf.at>
2018-05-13 19:53:59 +0200

    Implement basic rich text functionality

--- src/main/java/nodebook/controller/NodebookController.java
+++ src/main/java/nodebook/controller/NodebookController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ToolBar;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.URL;
@@ -12,17 +13,23 @@ import java.util.ResourceBundle;
 
 @FXMLController
 public class NodebookController implements Initializable {
+    private final NodeTreeComponent nodeTreeComponent;
+    private final ToolBarComponent toolBarComponent;
+    private final RichTextComponent richTextComponent;
     @FXML
     private TreeView<String> nodeTreeView;
     @FXML
     private ToolBar toolBar;
-    private final NodeTreeComponent nodeTreeComponent;
-    private final ToolBarComponent toolBarComponent;
+    @FXML
+    private AnchorPane richtextPane;
 
     @Autowired
-    public NodebookController(NodeTreeComponent nodeTreeComponent, ToolBarComponent toolBarComponent) {
+    public NodebookController(NodeTreeComponent nodeTreeComponent,
+                              ToolBarComponent toolBarComponent,
+                              RichTextComponent richTextComponent) {
         this.nodeTreeComponent = nodeTreeComponent;
         this.toolBarComponent = toolBarComponent;
+        this.richTextComponent = richTextComponent;
     }
 
     @FXML
@@ -30,5 +37,6 @@ public class NodebookController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         nodeTreeComponent.setTreeNodes(nodeTreeView);
         toolBarComponent.addButtons(toolBar);
+        richTextComponent.initalize(richtextPane);
     }
 }
--- src/main/java/nodebook/controller/ToolBarComponent.java
+++ src/main/java/nodebook/controller/ToolBarComponent.java
@@ -3,6 +3,7 @@ package nodebook.controller;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
 import javafx.scene.control.Tooltip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,18 +15,20 @@ public class ToolBarComponent {
             "bold",
             "italic",
             "underline",
-            "strike-through",
+            "strikethrough",
 //            "header-1",
 //            "header-2",
 //            "header-3",
     };
 
+    @Autowired
+    private RichTextComponent richTextComponent;
+
     public void addButtons(ToolBar toolBar) {
         for (String button : buttons) {
-            toolBar.getItems().add(createButton(button, null, ""));
+            toolBar.getItems().add(createButton(button, getAction(button), ""));
         }
     }
-
 
     private Button createButton(String styleClass, Runnable action, String toolTip) {
         Button button = new Button();
@@ -34,15 +37,30 @@ public class ToolBarComponent {
         button.setPrefHeight(25);
 
         button.getStyleClass().add(styleClass);
-//        button.setOnAction(evt -> {
-//            action.run();
+        button.setOnAction(evt -> {
+            action.run();
 //            area.requestFocus();
-//        });
+        });
 
         if (toolTip != null) {
             button.setTooltip(new Tooltip(toolTip));
         }
 
         return button;
+    }
+
+    private Runnable getAction(String button) {
+        switch (button) {
+            case "bold":
+                return richTextComponent::toggleBold;
+            case "italic":
+                return richTextComponent::toggleItalic;
+            case "underline":
+                return richTextComponent::toggleUnderline;
+            case "strikethrough":
+                return richTextComponent::toggleStrikethrough;
+            default:
+                return richTextComponent::toggleBold;
+        }
     }
 }
