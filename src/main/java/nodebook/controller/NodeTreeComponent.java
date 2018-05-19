458fce2a1c9b13a09ebe2c9c3e52e45d2565fa26
Roman Khassraf <roman@khassraf.at>
2018-05-19 16:42:52 +0200

    Add TreeNode icons

--- src/main/java/nodebook/controller/NodeTreeComponent.java
+++ src/main/java/nodebook/controller/NodeTreeComponent.java
@@ -2,15 +2,42 @@ package nodebook.controller;
 
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NodeTreeComponent {
     public void setTreeNodes(TreeView<String> nodeTreeView) {
-        TreeItem<String> rootItem = new TreeItem<>("Root");
-        rootItem.getChildren().add(new TreeItem<>("Test"));
-        rootItem.getChildren().add(new TreeItem<>("Test 2"));
-        rootItem.getChildren().add(new TreeItem<>("Test 3"));
+        TreeItem<String> rootItem = createTreeItem("Root", 0);
+        rootItem.getChildren().add(createTreeItem("Test", 1));
+        rootItem.getChildren().add(createTreeItem("Test 2", 1));
+        rootItem.getChildren().add(createTreeItem("Test 3", 1));
         nodeTreeView.setRoot(rootItem);
+    }
+
+    private TreeItem<String> createTreeItem(String name, int treeLevel) {
+        ImageView imageView = new ImageView(
+                new Image(getClass().getResourceAsStream(getIconName(treeLevel)))
+        );
+
+        return new TreeItem<>(name, imageView);
+    }
+
+    private String getIconName(int treeLevel) {
+        String color = "";
+        switch (treeLevel) {
+            case 0:
+                color = "red";
+                break;
+            case 1:
+                color = "blue";
+                break;
+            default:
+                color = "green";
+                break;
+        }
+
+        return "/images/dot-" + color + ".png";
     }
 }
