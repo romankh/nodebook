6bf92f6f149981d62c34b8c8099145c15803cf67
Roman Khassraf <roman@khassraf.at>
2018-05-26 12:52:16 +0200

    Add custom splash screen

--- src/main/java/nodebook/NodebookApplication.java
+++ src/main/java/nodebook/NodebookApplication.java
@@ -1,12 +1,13 @@
 package nodebook;
 
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+import nodebook.ui.AppSplashScreen;
 import nodebook.view.NodebookView;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NodebookApplication extends AbstractJavaFxApplicationSupport {
     public static void main(String[] args) {
-        launch(NodebookApplication.class, NodebookView.class, args);
+        launch(NodebookApplication.class, NodebookView.class, new AppSplashScreen(), args);
     }
 }
---
+++ src/main/java/nodebook/ui/AppSplashScreen.java
@@ -0,0 +1,11 @@
+package nodebook.ui;
+
+import de.felixroske.jfxsupport.SplashScreen;
+
+public class AppSplashScreen extends SplashScreen {
+
+    @Override
+    public String getImagePath() {
+        return "/images/splash.png";
+    }
+}
