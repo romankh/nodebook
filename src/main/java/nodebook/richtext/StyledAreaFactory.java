4d4b6f780f06e03f6ff8ed191b0e6d2727999696
Roman Khassraf <roman@khassraf.at>
2018-05-26 15:45:55 +0200

    Move rich text area instantiation to factory

---
+++ src/main/java/nodebook/richtext/StyledAreaFactory.java
@@ -0,0 +1,55 @@
+package nodebook.richtext;
+
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import nodebook.richtext.content.LinkedImage;
+import nodebook.richtext.content.LinkedImageOps;
+import nodebook.richtext.style.ParStyle;
+import nodebook.richtext.style.TextStyle;
+import org.fxmisc.richtext.GenericStyledArea;
+import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.StyledTextArea;
+import org.fxmisc.richtext.TextExt;
+import org.fxmisc.richtext.model.Codec;
+import org.fxmisc.richtext.model.SegmentOps;
+import org.fxmisc.richtext.model.StyledSegment;
+import org.fxmisc.richtext.model.TextOps;
+import org.reactfx.util.Either;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+public class StyledAreaFactory {
+    private static final TextOps<String, TextStyle> styledTextOps = SegmentOps.styledTextOps();
+    private static final LinkedImageOps<TextStyle> linkedImageOps = new LinkedImageOps<>();
+
+    public static GenericStyledArea<ParStyle, Either<String, LinkedImage>, TextStyle> getStyledTextArea() {
+        GenericStyledArea<ParStyle, Either<String, LinkedImage>, TextStyle> area = new GenericStyledArea<>(
+                // default paragraph style
+                ParStyle.EMPTY,
+                // paragraph style setter
+                (paragraph, style) -> paragraph.setStyle(style.toCss()),
+                // default segment style
+                TextStyle.EMPTY.updateFontSize(11).updateFontFamily("Sans").updateFontColor(Color.BLACK),
+                // segment operations
+                styledTextOps._or(linkedImageOps, (s1, s2) -> Optional.empty()),
+                // node factory
+                seg -> createNode(seg, (text, style) -> text.setStyle(style.toCss()))
+        );
+
+        area.setWrapText(true);
+        area.setStyleCodecs(
+                ParStyle.CODEC,
+                Codec.styledSegmentCodec(Codec.eitherCodec(Codec.STRING_CODEC, LinkedImage.codec()), TextStyle.CODEC));
+        area.setParagraphGraphicFactory(LineNumberFactory.get(area));
+        return area;
+    }
+
+    private static Node createNode(StyledSegment<Either<String, LinkedImage>, TextStyle> seg,
+                                   BiConsumer<? super TextExt, TextStyle> applyStyle) {
+        return seg.getSegment().unify(
+                text -> StyledTextArea.createStyledTextNode(text, seg.getStyle(), applyStyle),
+                LinkedImage::createNode
+        );
+    }
+}
--- src/main/java/nodebook/service/RichTextService.java
+++ src/main/java/nodebook/service/RichTextService.java
@@ -1,23 +1,15 @@
 package nodebook.service;
 
-import javafx.scene.Node;
 import javafx.scene.control.IndexRange;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import nodebook.richtext.StyledAreaFactory;
 import nodebook.richtext.content.LinkedImage;
-import nodebook.richtext.content.LinkedImageOps;
 import nodebook.richtext.style.ParStyle;
 import nodebook.richtext.style.TextStyle;
 import org.fxmisc.richtext.GenericStyledArea;
-import org.fxmisc.richtext.LineNumberFactory;
-import org.fxmisc.richtext.StyledTextArea;
-import org.fxmisc.richtext.TextExt;
-import org.fxmisc.richtext.model.Codec;
 import org.fxmisc.richtext.model.Paragraph;
-import org.fxmisc.richtext.model.SegmentOps;
 import org.fxmisc.richtext.model.StyleSpans;
-import org.fxmisc.richtext.model.StyledSegment;
-import org.fxmisc.richtext.model.TextOps;
 import org.fxmisc.richtext.model.TwoDimensional;
 import org.reactfx.util.Either;
 import org.springframework.stereotype.Service;
@@ -25,35 +17,20 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 @Service
 public class RichTextService {
-    private final TextOps<String, TextStyle> styledTextOps = SegmentOps.styledTextOps();
-    private final LinkedImageOps<TextStyle> linkedImageOps = new LinkedImageOps<>();
     private final String SEPARATOR = "--------------------------------------------------------------------------------";
-    private GenericStyledArea<ParStyle, Either<String, LinkedImage>, TextStyle> area = new GenericStyledArea<>(
-            ParStyle.EMPTY,                                                 // default paragraph style
-            (paragraph, style) -> paragraph.setStyle(style.toCss()),        // paragraph style setter
-            TextStyle.EMPTY.updateFontSize(12).updateFontFamily("Serif").updateFontColor(Color.BLACK),  // default segment style
-            styledTextOps._or(linkedImageOps, (s1, s2) -> Optional.empty()),                            // segment operations
-            seg -> createNode(seg, (text, style) -> text.setStyle(style.toCss())));                     // Node creator and segment style setter
-
-    {
-        area.setWrapText(true);
-        area.setStyleCodecs(
-                ParStyle.CODEC,
-                Codec.styledSegmentCodec(Codec.eitherCodec(Codec.STRING_CODEC, LinkedImage.codec()), TextStyle.CODEC));
-    }
+    private GenericStyledArea<ParStyle, Either<String, LinkedImage>, TextStyle> area = StyledAreaFactory
+            .getStyledTextArea();
 
     public void initalize(AnchorPane richtextPane) {
         AnchorPane.setTopAnchor(area, 0.0);
         AnchorPane.setLeftAnchor(area, 0.0);
         AnchorPane.setRightAnchor(area, 0.0);
         AnchorPane.setBottomAnchor(area, 0.0);
-        area.setParagraphGraphicFactory(LineNumberFactory.get(area));
+
         richtextPane.getChildren().add(area);
     }
 
@@ -191,13 +168,5 @@ public class RichTextService {
             area.setStyleSpans(selection.getStart(), newStyles);
             area.requestFocus();
         }
-    }
-
-    private Node createNode(StyledSegment<Either<String, LinkedImage>, TextStyle> seg,
-                            BiConsumer<? super TextExt, TextStyle> applyStyle) {
-        return seg.getSegment().unify(
-                text -> StyledTextArea.createStyledTextNode(text, seg.getStyle(), applyStyle),
-                LinkedImage::createNode
-        );
     }
 }
